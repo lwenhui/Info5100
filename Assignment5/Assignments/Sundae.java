d8e217c845fc8af70880c69741f9bb31f624a834
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-11-13 18:32:16 -0800

    comments

--- Assignment5/Assignments/Sundae.java
+++ Assignment5/Assignments/Sundae.java
@@ -2,7 +2,7 @@ public class Sundae extends IceCream {
 	private String nameOfTopping;
 	private int costOfTopping;
 	
-	public Sundae() {}
+	public Sundae() {} // this constructor is not required as we don't want to create items without attributes
 	public Sundae(String iceCreamName, int iceCreamCost, String nameOfTopping, int costOfTopping) { 
 		super(iceCreamName, iceCreamCost);
 		this.nameOfTopping = nameOfTopping;
